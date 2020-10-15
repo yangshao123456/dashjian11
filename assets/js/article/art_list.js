b27d96c2f1cde68a427926b8e3f5146ec0512e26
yangshao <3200547951@qq.com>
2020-10-15 09:28:26 +0800

    个人中心功能的开发

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,40 @@
+$(function () {
+    var layer = layui.layer
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+    var q = {
+        pagenum: 1,
+        pagesize: 2,
+        cate_id: '', // 文章分类的 Id
+        state: ''
+    }
+    initTable();
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+})
